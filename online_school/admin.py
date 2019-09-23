cffd798e829ecaeefccb8054c57b349b51f72618
Zaboev Andrey <zaboevai@yandex.ru>
2019-09-23 23:17:57 +0300

    1) added abstract mixin 2) added join table with many to many fields

--- online_school/admin.py
+++ online_school/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Course, TypeCourse, Teacher, Lesson, Student, StudentsGroup
+from .models import Course, TypeCourse, Teacher, Lesson, Student, StudentsGroup, TeacherCourse
+
+
+@admin.register(TeacherCourse)
+class TeacherCourseAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(TypeCourse)
@@ -9,7 +14,7 @@ class TypeCourseAdmin(admin.ModelAdmin):
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'desc', 'type_fk')
+    list_display = ('id', 'title', 'desc', 'type')
 
 
 @admin.register(Teacher)
@@ -19,14 +24,14 @@ class TeacherAdmin(admin.ModelAdmin):
 
 @admin.register(Lesson)
 class CourseLessonsAdmin(admin.ModelAdmin):
-    list_display = ('course_fk', 'id', 'title', 'desc', 'date', 'time', 'created')
+    list_display = ('course', 'id', 'title', 'desc', 'date', 'time', 'created')
 
 
 @admin.register(Student)
 class StudentsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'last_name', 'first_name', 'patronymic', 'e_mail', 'group_fk')
+    list_display = ('id', 'last_name', 'first_name', 'patronymic', 'e_mail', 'group')
 
 
 @admin.register(StudentsGroup)
 class StudentsGroupAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'course_fk')
+    list_display = ('id', 'name', 'course')
--- online_school/models.py
+++ online_school/models.py
@@ -1,7 +1,34 @@
 from django.db import models
 
 
-class TypeCourse(models.Model):
+class AbstractPeopleNamesMixin(models.Model):
+    class Meta:
+        abstract = True
+
+    last_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    patronymic = models.CharField(max_length=255)
+
+
+class AbstractTitleDescMixin(models.Model):
+    class Meta:
+        abstract = True
+
+    title = models.CharField(max_length=255)
+    desc = models.TextField(max_length=255)
+
+
+class AbstractDateTimeMixin(models.Model):
+    class Meta:
+        abstract = True
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class TypeCourse(AbstractDateTimeMixin):
+    class Meta:
+        ordering = ('id',)
 
     name = models.CharField(max_length=255)
 
@@ -9,76 +36,69 @@ class TypeCourse(models.Model):
         return f'{self.name}'
 
 
-class Teacher(models.Model):
-
+class Teacher(AbstractPeopleNamesMixin, AbstractDateTimeMixin):
     class Meta:
         ordering = ('id',)
 
-    last_name = models.CharField(max_length=255)
-    first_name = models.CharField(max_length=255)
-    patronymic = models.CharField(max_length=255)
-    course_fk = models.ManyToManyField('Course')
+    courses = models.ManyToManyField('Course', related_name='Teacher', through='TeacherCourse')
 
     def __str__(self):
         return f'{self.last_name} {self.first_name} {self.patronymic}'
 
 
-class Student(models.Model):
+class TeacherCourse(AbstractDateTimeMixin):
+    class Meta:
+        unique_together = (('teacher', 'course',),)
 
+    teacher = models.ForeignKey('Teacher', on_delete=models.CASCADE)
+    course = models.ForeignKey('Course', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.teacher_id} {self.course_id}'
+
+
+class Student(AbstractPeopleNamesMixin, AbstractDateTimeMixin):
     class Meta:
         ordering = ('id',)
 
-    last_name = models.CharField(max_length=255)
-    first_name = models.CharField(max_length=255)
-    patronymic = models.CharField(max_length=255)
     e_mail = models.CharField(max_length=255)
-    group_fk = models.ForeignKey('StudentsGroup', null=True, blank=True, on_delete=models.SET_NULL)
+    group = models.ForeignKey('StudentsGroup', null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'{self.last_name} {self.first_name} {self.patronymic}'
 
 
-class StudentsGroup(models.Model):
-
+class StudentsGroup(AbstractDateTimeMixin):
     class Meta:
-        ordering = ('course_fk','id')
+        ordering = ('course', 'id')
 
     name = models.CharField(max_length=255)
-    course_fk = models.ForeignKey('Course', null=True, blank=True, on_delete=models.SET_NULL)
+    course = models.ForeignKey('Course', null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
-        return f'{self.name} {self.course_fk}'
+        return f'{self.name} {self.course}'
 
 
-class Course(models.Model):
-
+class Course(AbstractTitleDescMixin, AbstractDateTimeMixin):
     class Meta:
         ordering = ('id',)
 
-    name = models.CharField(max_length=255)
-    desc = models.TextField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
-    type_fk = models.ForeignKey('TypeCourse', null=True, blank=True, on_delete=models.SET_NULL)
-    teacher_fk = models.ManyToManyField('Teacher')
+    type = models.ForeignKey('TypeCourse', null=True, blank=True, on_delete=models.SET_NULL)
+    teachers = models.ManyToManyField('Teacher', related_name='Course', through='TeacherCourse')
 
     def __str__(self):
-        return f' {self.name} ({self.type_fk})'
+        return f' {self.title} ({self.type})'
 
 
-class Lesson(models.Model):
-
+class Lesson(AbstractTitleDescMixin, AbstractDateTimeMixin):
     class Meta:
-        ordering = ('course_fk', 'id')
+        ordering = ('course', 'id')
 
-    title = models.CharField(max_length=255)
-    desc = models.TextField(max_length=255)
-
-    course_fk = models.ForeignKey('Course', null=True, blank=True, on_delete=models.CASCADE)
-    teacher_fk = models.ForeignKey('Teacher', null=True, blank=True, on_delete=models.CASCADE)
+    course = models.ForeignKey('Course', null=True, blank=True, on_delete=models.CASCADE)
+    teacher = models.ForeignKey('Teacher', null=True, blank=True, on_delete=models.CASCADE)
     date = models.DateField()
     time = models.TimeField()
-
-    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.title} '
